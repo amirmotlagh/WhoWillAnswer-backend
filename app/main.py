b6d745e5f5ffa190f1f0c856ca28548a77b341a3
Amir Alaghmandan <amir.amotlagh@gmail.com>
2025-10-28 11:50:16 +0330

    fix(app): replace lifespan with on_event which is depricated

--- app/main.py
+++ app/main.py
@@ -1,26 +1,23 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
-app = FastAPI()
 
-@app.on_event("startup")
-async def startup_event():
-    #TODO: Add your startup logic here
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # TODO: Add your startup logic here
     # For example, you might want to connect to a database
     # db.connect()
     # Or initialize some resources
-    #  resource.initialize()
-    # For now, we'll just print a message, later it can be replaced with actual logic
-    print("Application is starting up...")
-
-
-@app.on_event("shutdown")
-async def shutdown_event():
-    #TODO: Add your shutdown logic here
+    # resource.initialize()
+    yield
+    # TODO: Add your shutdown logic here
     # For example, you might want to disconnect from a database
     # db.disconnect()
     # Or release some resources
     # resource.release()
-    print("Application is shutting down...")
+
+app = FastAPI(lifespan=lifespan)
 
 # app.include_router(...)  # Add your routers here
 # e.g., app.include_router(user_router)
