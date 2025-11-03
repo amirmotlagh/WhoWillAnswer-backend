1e3c7ea88e9bdda44f3dd6fcfd464c71f0d51d67
shirin.shahsavani <s.shahsavani@fanap-infra.ir>
2025-11-03 09:53:39 +0330

    feat(config): add Redis configuration options (host, port, password, db index)

--- app/config.py
+++ app/config.py
@@ -11,8 +11,14 @@ if not os.path.exists(ENV_FILE):
     print(f"Warning: Environment file '{ENV_FILE}' does not exist.", file=sys.stderr)
 
 class Settings(BaseSettings):
+    ####Redis######
     DEBUG: bool = False
-    model_config = {
+    REDIS_HOST: str = 'localhost'
+    REDIS_PORT: str = '6379'
+    REDIS_PASSWORD: str | None = None
+    REDIS_DB: int = 0
+
+model_config = {
         "env_file": ENV_FILE
     }
 settings = Settings()
