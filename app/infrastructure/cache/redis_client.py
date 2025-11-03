89784ac42142f6ad972ab8cb7746c35d028e3f05
shirin.shahsavani <s.shahsavani@fanap-infra.ir>
2025-11-03 10:14:52 +0330

    feat(cache): add Redis connection pooling.

--- app/infrastructure/cache/redis_client.py
+++ app/infrastructure/cache/redis_client.py
@@ -2,8 +2,11 @@ import redis.asyncio as redis
 from app.config import  settings
 
 
-redis_client = redis.Redis(
+pool = redis.ConnectionPool(
     host=settings.REDIS_HOST,
     port=settings.REDIS_PORT,
-    decode_responses=True
+    decode_responses=True,
+    max_connections=10
 )
+
+redis_client = redis.Redis(connection_pool=pool)
