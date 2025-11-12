e7586b11c19679ef5b0f3acf87b41a9809086fb8
shirin.shahsavani <s.shahsavani@fanap-infra.ir>
2025-11-12 10:02:34 +0330

    fix(health): Return json response instead of True and False.

--- app/api/v1/routes/health.py
+++ app/api/v1/routes/health.py
@@ -1,4 +1,6 @@
 import redis
+from starlette.responses import JSONResponse
+
 from app.config import settings
 
 r = redis.Redis(host=settings.REDIS_HOST,socket_timeout=1)
@@ -6,10 +8,10 @@ def is_redis_available():
     try:
         r.ping()
         ##TODO: add logging
-        return True
+        return JSONResponse(status_code=200, content={"status": "ok"})
     except Exception:
         ##TODO add logging
-        return False
+        return JSONResponse(status_code=503, content={"status": "error"})
 
 
 
