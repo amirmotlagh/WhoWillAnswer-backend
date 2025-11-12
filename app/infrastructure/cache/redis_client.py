b9e4bcd5a2bafa1aa9392df5a3d0c93e2f6a8036
shirin.shahsavani <s.shahsavani@fanap-infra.ir>
2025-11-12 10:12:37 +0330

    fix(redis_client): move max_connections to settings.

--- app/infrastructure/cache/redis_client.py
+++ app/infrastructure/cache/redis_client.py
@@ -6,7 +6,7 @@ pool = redis.ConnectionPool(
     host=settings.REDIS_HOST,
     port=settings.REDIS_PORT,
     decode_responses=True,
-    max_connections=10
+    max_connections=settings.REDIS_MAX_CONNECTIONS
 )
 
 redis_client = redis.Redis(connection_pool=pool)
