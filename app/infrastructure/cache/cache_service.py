d7d579a21b9140bfa52f0e52c3c51c72ee615a91
shirin.shahsavani <s.shahsavani@fanap-infra.ir>
2025-11-18 15:35:02 +0330

    feat(cache): add JSON serialization, error handling, and boolean return values to CacheService

--- app/infrastructure/cache/cache_service.py
+++ app/infrastructure/cache/cache_service.py
@@ -1,22 +1,54 @@
 from app.infrastructure.cache.redis_client import redis_client
+import json
 
 class CacheService:
     @staticmethod
-    async def set(key: str, value: str, expire: int | None = None):
+    async def set(key: str, value: str, expire: int | None = None, serialize: bool = True) -> bool:
         client = redis_client.get_client()
-        await client.set(key, value, ex=expire)
+
+        try:
+            if serialize:
+                value = json.dumps(value)
+            await client.set(key, value, ex=expire)
+            return True
+        except Exception:
+            # TODO add logging
+            return False
+
+
 
     @staticmethod
-    async def get(key: str):
+    async def get(key: str,serialize: bool = True):
         client = redis_client.get_client()
-        return await client.get(key)
+        try:
+            data = await client.get(key)
+            if data is None:
+                return None
+
+            if serialize:
+                return json.loads(data)
+
+            return data
+        except Exception:
+            # TODO add logging
+            return None
 
     @staticmethod
-    async def delete(key: str):
+    async def delete(key: str) -> bool:
         client = redis_client.get_client()
-        await client.delete(key)
 
+        try:
+            await client.delete(key)
+            return True
+        except Exception:
+            # TODO add logging
+            return False
     @staticmethod
-    async def exists(key: str):
+    async def exists(key: str) -> bool:
         client = redis_client.get_client()
-        return await client.exists(key)
+
+        try:
+            return bool(await client.exists(key))
+        except Exception:
+            # TODO add logging
+            return False
