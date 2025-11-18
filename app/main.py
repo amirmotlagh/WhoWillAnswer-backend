70da0f4bf16c4e11cf7ab096b3c380328b72021b
shirin.shahsavani <s.shahsavani@fanap-infra.ir>
2025-11-18 12:41:18 +0330

    refactor(main):import redis_client to main.py

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
 from contextlib import asynccontextmanager
-
+from app.infrastructure.cache.redis_client import redis_client
 from fastapi import FastAPI
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # TODO: Add your startup logic here
+
     # For example, you might want to connect to a database
     # db.connect()
     # Or initialize some resources
