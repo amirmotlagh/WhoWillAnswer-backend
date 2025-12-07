bdd8dc808d4e9beb71469563a8f336caec1edff2
shirin.shahsavani <s.shahsavani@fanap-infra.ir>
2025-12-07 12:42:54 +0330

    add(session): add connecting to sql's URL.

--- app/infrastructure/database/session.py
+++ app/infrastructure/database/session.py
@@ -1,3 +1,7 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from app.config import settings
 
+DATABASE_URL = (
+    f"postgresql+asyncpg://{settings.DB_USER}:{settings.DB_PASSWORD}"
+    f"@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}"
+)
