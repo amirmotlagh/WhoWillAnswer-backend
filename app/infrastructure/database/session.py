1e192a2632ba985c3aa22eb43af5b511b32875d9
shirin.shahsavani <s.shahsavani@fanap-infra.ir>
2025-12-07 12:45:59 +0330

    add(session): create engine in order to handle connections and connection pool.

--- app/infrastructure/database/session.py
+++ app/infrastructure/database/session.py
@@ -5,3 +5,10 @@ DATABASE_URL = (
     f"postgresql+asyncpg://{settings.DB_USER}:{settings.DB_PASSWORD}"
     f"@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}"
 )
+
+engine = create_async_engine(
+    DATABASE_URL,
+    echo=settings.DB_ECHO,
+    pool_size=settings.DB_POOL_SIZE,
+    max_overflow=settings.DB_MAX_OVERFLOW,
+)
