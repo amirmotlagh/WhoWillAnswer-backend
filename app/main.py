8f4107dfeb010437314b007ab78afd9fd787ad3a
Amir Motlagh <amir.amotlagh@gmail.com>
2026-02-15 10:29:57 +0330

    fix(app): update logger config and start at main

--- app/main.py
+++ app/main.py
@@ -1,18 +1,21 @@
 from contextlib import asynccontextmanager
 from app.infrastructure.cache.redis_client import redis_client
 from fastapi import FastAPI
-import logging.config
-from app.logger.py import logging_config
+from app.logger import setup_logging, get_logger
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    setup_logging()
+    logger = get_logger("uvicorn.error")
+    logger.info("Application startup: Logging configured")
     try:
         await redis_client.connect()
     except Exception as e:
-        ##TODO add logging
+        logger.error(f"Failed to connect to Redis: {e}")
         raise RuntimeError(f"Failed to connect to Redis: {e}")
 
-
+    logger.info("Application startup: App resources initialized")
+    
     yield
     await redis_client.disconnect()
 
