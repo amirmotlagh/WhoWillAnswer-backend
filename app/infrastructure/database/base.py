f6f6646273d3f4e368f291945c13fed42832c0a1
Amir Motlagh <amir.amotlagh@gmail.com>
2026-05-23 09:42:30 +0330

    fix(infrastructure): fix migration of is_correct field of Answers, update base setup of db

--- app/infrastructure/database/base.py
+++ app/infrastructure/database/base.py
@@ -20,25 +20,32 @@ DATABASE_URL = settings.DATABASE_URL
 if not DATABASE_URL:
     raise ValueError("DATABASE_URL environment variable is required")
 
+pool_class = get_pool_class(settings.DB_POOL_CLASS)
+engine_kwargs = {
+    "echo":settings.DEBUG,
+    "poolclass":pool_class
+}
+if pool_class is AsyncAdaptedQueuePool:
+    engine_kwargs.update(
+        pool_size=settings.DB_POOL_SIZE,
+        max_overflow=settings.DB_MAX_OVERFLOW,
+        pool_timeout=settings.DB_POOL_TIMEOUT,
+    )
 # Create async engine with connection pooling
 engine = create_async_engine(
     DATABASE_URL,
-    echo=settings.DEBUG,
-    pool_size=settings.DB_POOL_SIZE,
-    max_overflow=settings.DB_MAX_OVERFLOW,
-    pool_timeout=settings.DB_POOL_TIMEOUT,
-    poolclass=get_pool_class(settings.DB_POOL_CLASS),
+    **engine_kwargs,
     # Performance optimizations
-    # pool_pre_ping=True,  # Validate connections
-    # pool_reset_on_return="commit",  # Reset connections
+    pool_pre_ping=True,  # Validate connections
+    pool_reset_on_return="commit",  # Reset connections
 
     # Connection arguments
-    # connect_args={
-        # "server_settings": {
-            # "application_name": settings.app_name,
-            # "jit": "off",  # Disable JIT for better connection times
-        # }
-    # }
+    connect_args={
+        "server_settings": {
+            "application_name": "WhoWillAnswer",
+            "jit": "off",  # Disable JIT for better connection times
+        }
+    }
 )
 
 # Database base class
