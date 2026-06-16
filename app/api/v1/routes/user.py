fe974758eb6ffec599bf47a525d9eb8e146bca05
Amir Motlagh <amir.amotlagh@gmail.com>
2026-06-16 14:44:33 +0330

    fix(routes): handle exceptions

--- app/api/v1/routes/user.py
+++ app/api/v1/routes/user.py
@@ -1,20 +1,23 @@
 
+from typing import Annotated
 
 from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core.services import user_service
 from app.infrastructure.database.repositories.user_repository import UserRepository
 from app.infrastructure.database.session import get_database_session
 from app.logger import get_logger
-from app.schemas.user import UserCreate, UserResponse
+from app.schemas.response import UserCreateResponse
+from app.schemas.user import UserCreate
 
 
 logger = get_logger(__name__)
 
 user_router = APIRouter(prefix="/user", tags=["user"])
 
-@user_router.post("/create/", summary="Create a new user", status_code=201)
-async def create_user(user_data: UserCreate, session=Depends(get_database_session)):
+@user_router.post("/create/", summary="Create a new user", status_code=201, response_model=UserCreateResponse)
+async def create_user(user_data: UserCreate, session: Annotated[AsyncSession, Depends(get_database_session)]):
     user_repo = UserRepository(session)
     user = await user_service.create_user(user_data, user_repo)
     
